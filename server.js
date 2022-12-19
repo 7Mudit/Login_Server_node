c4a27eb82f4aa692139905f55f5af94bdc22306a
Mudit Kapoor <muditk441@gmail.com>
2022-12-19 22:46:33 +0530

    Cors

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require('./config/db')
 const app=require('express')();
 const port=process.env.PORT || 3000
 
+//cors
+const cors=require("cors");
+app.use(cors());
+
 const UserRouter=require('./api/User')
 
 // for accepting post form data
